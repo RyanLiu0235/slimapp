0b9cd1ddf1e679a5454e580487bd02890ed825ad
stop2stare <emitremmus0235@163.com>
2018-02-28 10:51:22 +0800

    diff and patch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 var seb = require('seb-vdom')
 var h = seb.h
-var render = seb.render
+
+function isString(obj) {
+  // bug of seb in VNode constructor
+  return typeof obj === 'string' || typeof obj === 'number'
+}
 
 /**
  * app
@@ -13,10 +17,55 @@ var render = seb.render
 function app(view, actions, state, container) {
   var wiredActions = wireStateToActions(actions, state)
   var rootNode = view(wiredActions, state)
-  var rootEl = render(rootNode)
-  container.appendChild(rootEl)
+  var rootEl = (container && container.childNodes[0]) || null
+  rootEl = diffAndPatch(container, rootEl, null, rootNode)
 
   return wiredActions
+
+  function render(vnode) {
+    if (isString(vnode)) {
+      return document.createTextNode(vnode)
+    } else {
+      // 1. tag
+      var el = document.createElement(vnode.tagName)
+
+      // 2. add props
+      var props = vnode.props
+      var propNames = Object.keys(props)
+      var prop, value
+      for (var i = 0; i < propNames.length; i++) {
+        prop = propNames[i]
+        value = props[prop]
+        if (prop === 'key') continue
+        if (typeof value === 'function') {
+          // bind this function to el
+          // prop expects event name lick `onclick`
+          if (prop in el) {
+            el[prop] = value
+          }
+        } else {
+          el.setAttribute(prop, value)
+        }
+      }
+
+      // 3. children
+      var children = vnode.children
+      var child
+      for (var j = 0; j < children.length; j++) {
+        child = children[j]
+        // if child is text
+        if (['string', 'number'].indexOf(typeof child) > -1) {
+          var textNode = document.createTextNode(child)
+          el.appendChild(textNode)
+        } else {
+          var childNode = render(child)
+          el.appendChild(childNode)
+        }
+      }
+
+      return el
+    }
+  }
 
   function wireStateToActions(actions, state) {
     var _actions = {}
@@ -26,8 +75,7 @@ function app(view, actions, state, container) {
       ;(function(key, handler) {
         _actions[key] = function(payload) {
           handler(payload)(state)
-
-          updateView()
+          diffAndPatch(container, rootEl, rootNode, rootNode = view(wiredActions, state))
         }
       })(key, handler)
     }
@@ -35,13 +83,130 @@ function app(view, actions, state, container) {
     return _actions
   }
 
-  function updateView() {
-    var oldEl = rootEl
-    rootNode = view(wiredActions, state)
-    rootEl = render(rootNode)
+  function diffAndPatch(parent, oldEl, oldNode, newNode) {
+    if (oldNode === null) {
+      // 1. brand new node
+      oldEl = parent.appendChild(render(newNode))
+    } else if (
+      isString(oldNode) && isString(newNode) &&
+      oldNode !== newNode
+    ) {
+      // 2. both text node and changed
+      parent.textContent = newNode
+    } else if (
+      oldNode.tagName &&
+      oldNode.tagName === newNode.tagName &&
+      oldNode.key === newNode.key
+    ) {
+      // 3. tag remains still, we'll check if attrs and children have been changed
+      // check attrs
+      updateAttrs(oldEl, oldNode.props, newNode.props)
 
-    container.removeChild(oldEl)
-    container.appendChild(rootEl)
+      // check children
+      var oldChildren = oldNode.children
+      var newChildren = newNode.children
+
+      // store all the old keys
+      var oldChildrenMap = []
+      var key
+      for (var m = 0; m < oldChildren.length; m++) {
+        key = oldChildren[m].key
+        if (key !== undefined) {
+          oldChildrenMap[key] = [oldEl.childNodes[m], oldChildren[m]]
+        }
+      }
+
+      // go through newChildren
+      var i = 0 // cursor for newNode
+      var j = 0 // cursor for oldNode
+      var oldKeyCache = {}
+      var oldChild, newChild, oldKey, newKey
+
+      // go through newChildren
+      while (i < newChildren.length) {
+        newChild = newChildren[i]
+        oldChild = oldChildren[j]
+
+        if (oldChild === undefined) {
+          oldEl.insertBefore(render(newChild), null)
+          i++
+          continue
+        }
+
+        newKey = newChild.key
+        oldKey = oldChild.key
+
+        if (newKey === undefined) {
+          if (oldKey === undefined) {
+            diffAndPatch(oldEl, oldEl.childNodes[i], oldChild, newChild)
+            j++
+          }
+          i++
+        } else {
+          if (oldKeyCache[newKey]) {
+            // if this VNode has been stored before, apply it
+            oldEl.insertBefore(render(oldKeyCache[newKey]), render(oldChild))
+            i++
+          } else if (oldKey === newKey) {
+            // if VNode has not changed, patch directly
+            diffAndPatch(oldEl, oldChildrenMap[oldKey][0], oldChild, newChild)
+            i++
+            j++
+          } else {
+            // go through oldChildren till we find the node with same key,
+            // if not, create it with newChild and insert
+            while (j < oldChildren.length) {
+              oldChild = oldChildren[j]
+              oldKey = oldChild.key
+
+              if (oldKey === newKey) {
+                diffAndPatch(oldEl, oldChildrenMap[oldKey][0], oldChild, newChild)
+                i++
+                j++
+                break
+              } else {
+                oldKeyCache[oldKey] = oldChild
+                oldChildren.splice(j, 1)
+                oldEl.removeChild(oldChildrenMap[oldKey][0])
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return oldEl
+  }
+
+  function updateAttrs(el, oldProps, newProps) {
+    var allProps = Object.assign({}, oldProps, newProps)
+    var propsKeys = Object.keys(allProps)
+
+    var key
+    var propsPatches = {}
+    for (var i = 0; i < propsKeys.length; i++) {
+      key = propsKeys[i]
+      if (key === 'key') continue
+      if (newProps[key] !== oldProps[key]) {
+        propsPatches[key] = newProps[key]
+      }
+    }
+
+    var props = Object.keys(propsPatches)
+    if (props.length > 0) {
+      var prop, value
+      for (var j = 0; j < props.length; j++) {
+        prop = props[j]
+        value = propsPatches[prop]
+        if (typeof value === 'function') {
+          el[prop] = value
+        } else if (value === false || value === undefined) {
+          el.removeAttribute(prop)
+        } else {
+          el.setAttribute(prop, value)
+        }
+      }
+    }
   }
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -6,7 +6,18 @@ beforeEach(function() {
   document.body.innerHTML = ''
 })
 
-test('compile', function() {
+test('compile and render with text node', function() {
+  var view = function(actions, state) {
+    return 'test'
+  }
+  var actions = {}
+  var state = {}
+  app(view, actions, state, document.body)
+
+  expect(document.body.innerHTML).toBe('test')
+})
+
+test('compile and render', function() {
   var view = function(actions, state) {
     return h('p', {}, [state.a])
   }
@@ -21,7 +32,11 @@ test('compile', function() {
 
 test('update', function() {
   var view = function(actions, state) {
-    return h('p', {}, [state.a])
+    return h('p', {
+      onclick: function() {
+        console.log('clicked!')
+      }
+    }, [state.a])
   }
   var actions = {
     add: function() {
@@ -37,4 +52,75 @@ test('update', function() {
   vm.add()
 
   expect(document.body.innerHTML).toBe('<p>2</p>')
+})
+
+test('children reorder', function() {
+  var view = function(actions, state) {
+    return h('ol', {}, state.list.map(item => h('li', { key: item.key }, [item.text])))
+  }
+  var actions = {
+    add: function(payload) {
+      return function(state) {
+        state.list.push(payload)
+      }
+    },
+    del: function(key) {
+      return function(state) {
+        var index = state.list.findIndex(item => item.key === key)
+        state.list.splice(index, 1)
+      }
+    }
+  }
+  var state = {
+    list: [{
+      key: 1,
+      text: '1'
+    }, {
+      key: 2,
+      text: '2'
+    }]
+  }
+  var vm = app(view, actions, state, document.body)
+  vm.add({
+    key: 3,
+    text: '3'
+  })
+  vm.del(2)
+
+  expect(document.body.innerHTML).toBe(
+    '<ol>' +
+    '<li>1</li>' +
+    '<li>3</li>' +
+    '</ol>'
+  )
+})
+
+test('attrs update', function() {
+  var view = function(actions, state) {
+    return h('p', {
+      name: state.name,
+      class: state.className
+    }, 'test')
+  }
+  var actions = {
+    rename: function(name) {
+      return function(state) {
+        state.name = name
+      }
+    },
+    reclass: function(classes) {
+      return function(state) {
+        state.className = classes
+      }
+    }
+  }
+  var state = {
+    name: 'test',
+    className: 'classes'
+  }
+  var vm = app(view, actions, state, document.body)
+  vm.rename('test-again')
+  vm.reclass()
+
+  expect(document.body.innerHTML).toBe('<p name="test-again">test</p>')
 })
